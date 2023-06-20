5febf1a5550dd0d223987b3adff48d9bf579efca
Christian DOBE <christiandobe@hotmail.fr>
2023-06-20 22:20:15 +0200

    nouvel ajout

--- src/app/pages/create-registration/create-registration.component.ts
+++ src/app/pages/create-registration/create-registration.component.ts
@@ -1,14 +1,3 @@
-// import { Component } from '@angular/core';
-//
-// @Component({
-//   selector: 'app-create-registration',
-//   templateUrl: './create-registration.component.html',
-//   styleUrls: ['./create-registration.component.css']
-// })
-// export class CreateRegistrationComponent {
-//
-// }
-
 
 
 import {User} from "../../models/register.model";
